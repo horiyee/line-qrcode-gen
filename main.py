8b4ef658c89f22700b3c9b23ad4c30ed59fb1002
horri1520 <kh1520.shsf@gmail.com>
2020-10-20 00:32:01 +0900

    [fix] fix usage of API

--- main.py
+++ main.py
@@ -68,13 +68,15 @@ def handle_message(event):
     img_url = 'https://line-qrcode-gen.herokuapp.com/{}'.format(img_path)
     line_bot_api.reply_message(
         event.reply_token,
-        # TextSendMessage(
-        #     text='「{}」をQRコードに変換しました！'.format(message)
-        # ),
-        ImageSendMessage(
-            original_content_url=img_url,
-            preview_image_url=img_url,
-        )
+        [
+            TextSendMessage(
+                text='「{}」をQRコードに変換しました！'.format(message)
+            ),
+            ImageSendMessage(
+                original_content_url=img_url,
+                preview_image_url=img_url,
+            )
+        ]
     )
 
 
