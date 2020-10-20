7699f5ab0b24e9ecbed753f53539558478e46f14
horri1520 <kh1520.shsf@gmail.com>
2020-10-20 13:24:35 +0900

    [add] implement error handling works well

--- main.py
+++ main.py
@@ -97,20 +97,15 @@ def handle_message(event):
             ]
         )
     except Exception as error:
-        if type(error) == 'DataOverflowError':
+        if str(type(error)) == 'qrcode.exceptions.DataOverflowError':
             line_bot_api.reply_message(
                 event.reply_token,
-                [
-                    TextSendMessage(text='そんなに長い文章は変換できません！！'),
-                ]
+                TextSendMessage(text='そんなに長い文章は変換できません！！'),
             )
         else:
             line_bot_api.reply_message(
                 event.reply_token,
-                [
-                    TextSendMessage(text='エラーが発生しました。もう一度送ってみてね！'),
-                    TextSendMessage(text=str(type(error)))
-                ]
+                TextSendMessage(text='エラーが発生しました。。。'),
             )
 
 
